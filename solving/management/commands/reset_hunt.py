ec7bd9a9388e9f352c5513089a7fb4449c0d9adc
David Turner <novalis@novalis.org>
2012-01-08 15:57:33 -0500

    add debugging tool

---
+++ solving/management/commands/reset_hunt.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from hunt.solving.models import Team, TeamUnlock, Meta, EventPointToken
+
+import os
+import subprocess
+
+class Command(BaseCommand):
+    help = """Reset the hunt status"""
+
+    def handle(self, *args, **options):
+        TeamUnlock.objects.all().delete()
+        EventPointToken.objects.all().delete()
+
+        for team in Team.objects.all():
+            team.points = 0
+            team.nsolves = 0
+            team.event_points = 0
+            team.save()
+
+            team_release_path = os.path.join(settings.TEAM_PATH, team.id)
+            if len(team_release_path) < 10:
+                #this might be unsafe
+                print "possibly unsafe to remove " + team_release_path
+                return
+
+            if os.path.exists(team_release_path):
+                subprocess.call(["rm", "-rf", team_release_path])
+
+        Meta.objects.all().delete()
