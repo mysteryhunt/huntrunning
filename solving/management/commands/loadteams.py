cd54a4cf6d6533f7e79a55c2eb05bdf931750180
David Turner <novalis@novalis.org>
2012-01-09 00:44:06 -0500

    read location from hunt sheet (thanks, Jeff)

--- solving/management/commands/loadteams.py
+++ solving/management/commands/loadteams.py
@@ -12,13 +12,13 @@ class Command(BaseCommand):
         teamcsv = args[0]
         f = csv.reader(open(teamcsv))
         for line in f:
-            if len(line) < 18:
-                line = line + [""] * (18-len(line))
-            _, name, email, _, username, password, _, _, _, _, _, _, _, _, _, _, _, phones = line[:18]
+            if len(line) < 19:
+                line = line + [""] * (19-len(line))
+            _, name, email, _, username, password, _, _, _, _, _, _, _, _, _, _, _, phones,location = line[:19]
             if username == "Username":
                 #header
                 continue
-            team = Team(id=username,name=name,password=password,email=email)
+            team = Team(id=username,name=name,password=password,email=email,location=location)
             team.save()
             team.phone_set.all().delete()
             for phone in phones.split("\n"):
