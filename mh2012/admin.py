b1864804af2a82bb87cb9a75f2829741677ded7b
David Turner <novalis@novalis.org>
2012-01-11 19:03:35 -0500

    aargh, Django is immensely irritating

--- mh2012/admin.py
+++ mh2012/admin.py
@@ -8,6 +8,6 @@ from models import *
 
 class ShowProducedAdmin(admin.ModelAdmin):
     fields = ('team', 'round', 'release_at', 'do_not_release_puzzle_yet', 'point_released', 'puzzle_released')
-    readonly_fields = ('point_released', 'puzzle_released')
+    readonly_fields = ('release_at', 'point_released', 'puzzle_released')
 
 admin.site.register(ShowProduced, ShowProducedAdmin)
