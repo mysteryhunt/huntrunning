00f2fad9f2ab3ed65353aa3963654b7559c95f2e
David Turner <novalis@novalis.org>
2012-01-12 20:01:59 -0500

    assign solved puzzles to a var

--- solving/models.py
+++ solving/models.py
@@ -281,7 +281,9 @@ def post_save_answer_request(sender, instance=None, **kwargs):
         solved_path = os.path.join(team.team_path, "solved.js")
         f = open(solved_path + ".tmp", "w")
         solved = dict((solved.puzzle.title, solved.puzzle.id) for solved in Solved.objects.filter(team=team))
+        f.write("var puzzles_solved = ")
         f.write(json.dumps(solved))
+        f.write(";")
         f.close()
         os.rename(solved_path + ".tmp", solved_path)
 
